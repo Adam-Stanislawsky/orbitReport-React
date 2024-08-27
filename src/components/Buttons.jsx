be0226c0f7bb092440424cc16fb58dc6a8341a4d
Adam Stanislawsky <adamstanislawsky@Adams-MBP.attlocal.net>
2024-08-26 20:15:49 -0500

    testing

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,4 +1,14 @@
+import satData from "./components/satData.jsx";
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
+  {displaySats.map((sat, id) => {
+    return (
+      <button onClick={() => filterByType(sat)} key={id}>
+        {sat} Orbit
+      </button>
+    );
+  })}
+  //code continues
   return (
     <div>
       <button>Placeholder Button</button>
