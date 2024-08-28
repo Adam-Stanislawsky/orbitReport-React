d61a36a5bd364d34b3f75c87efff0e624d0ceddd
Adam Stanislawsky <adamstanislawsky@Adams-MBP.attlocal.net>
2024-08-27 22:49:14 -0500

    completed button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
+
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
 import Banner from "./components/Banner";
-
-import { useState } from "react";
 import satData from "./components/satData.jsx";
+
+
+
 
 function App() {
   const [sat, setSat] = useState(satData)
-  const displaySats = [...new Set(satData.map((data) => data.orbitType))];
+  //const displaySats = [...new Set(satData.map((data) => data.orbitType))]; CHANGED IN BELOW LINE TO orbitTypes per Carrie
+  const orbitTypes = [...new Set(satData.map((data) => data.orbitType))];
 
   const filterByType = (currentType) => {
     const displaySats = satData.filter((newSatDisplay) => {
@@ -22,7 +26,8 @@ function App() {
       <Buttons 
         filterByType={filterByType}
         setSat={setSat}
-        displaySats={displaySats}
+        //displaySats={displaySats} changed to orbitTypes below per carrie
+        orbitTypes={orbitTypes} //renamed
       />
       <Table sat={sat}/>
     </div>
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,20 +1,22 @@
-import satData from "./components/satData.jsx";
+import satData from "./satData.jsx";
 
-const Buttons = ({ filterByType, setSat, displaySats }) => {
-  {displaySats.map((sat, id) => {
-    return (
-      <button onClick={() => filterByType(sat)} key={id}>
-        {sat} Orbit
-      </button>
-    );
-  })}
-  //code continues
+//const Buttons = ({ filterByType, setSat, displaySats }) => {    changed displaySats to orbitTypes per carrie below
+const Buttons = ({ filterByType, setSat, orbitTypes }) => {
+  
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+      {orbitTypes.map((type, index) => {
+        return (
+          <button onClick={() => filterByType(type)} key={index}>
+            {type} Orbit
+          </button>
+          );
+        })}
+      <button onClick={() => setSat(satData)}>All Orbits</button>
     </div>
   );
 };
 
 export default Buttons;
+
+
